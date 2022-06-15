bb2d9ec1dc67a8f18808c1b7f720f807b385279a
Sriharsha Venkata Raghu Kalluru <raghuk.vit@gmail.com>
2022-06-15 08:28:17 +0530

    Update server.js

--- server.js
+++ server.js
@@ -243,6 +243,7 @@ redisClient.on('ready', (r) => {
     logger.info('Redis READY', r);
 });
 
+if (process.env.MONGO == 'true') {
 // set up Mongo
 function mongoConnect() {
     return new Promise((resolve, reject) => {
@@ -259,6 +260,33 @@ function mongoConnect() {
         });
     });
 }
+}
+
+if (process.env.DOCUMENTDB == 'true') {
+function mongoConnect() {
+    return new Promise((resolve, reject) => {
+    var mongoURL = process.env.MONGO_URL || 'mongodb://username:password@mongodb:27017/users?tls=true&replicaSet=rs0&readPreference=secondaryPreferred&retryWrites=false';
+    var client = mongoClient.connect(mongoURL,
+      {
+        // Mutable & Immutable
+        //tlsCAFile: `/home/roboshop/catalogue/rds-combined-ca-bundle.pem` //Specify the DocDB; cert
+        // Container
+        tlsCAFile: `/home/roboshop/catalogue/rds-combined-ca-bundle.pem` //Specify the DocDB; cert
+    }, (error, client) => {
+    if(error) {
+        reject(error);
+    } else {
+        db = client.db('users');
+        usersCollection = db.collection('users');
+        ordersCollection = db.collection('orders');
+        resolve('connected');
+    }
+});
+});
+}
+}
+
+
 
 function mongoLoop() {
     mongoConnect().then((r) => {
